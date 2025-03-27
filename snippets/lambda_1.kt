e67dcf4123c24a80f09e208977973460a15445be
Dirk Arnez <smalldirkalex@gmail.com>
2025-03-27 16:09:19 +0800

    - update lambda_1.kt

--- snippets/lambda_1.kt
+++ snippets/lambda_1.kt
@@ -9,4 +9,10 @@ fun main() {
 
     // [Higher-order functions and lambdas | Kotlin Documentation](https://kotlinlang.org/docs/lambdas.html#it-implicit-name-of-a-single-parameter)
      { it.length == 5 }
+    
+    val list = listOf(1, 2, 3)
+    println("Hello, world!!! ${list.filter({it > 1})}")
+    println("Hello, world!!! ${list.filter({a -> a > 1})}")
+    println("Hello, world!!! ${list.filter{a -> a % 2 == 0}}")
+    println("Hello, world!!! ${list.filter(fun(a: Int): Boolean { return a > 0 })}")
 }
