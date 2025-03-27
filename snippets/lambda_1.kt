89398f80837286a39f934f99ee81fe2b2ccd0f4c
Dirk Arnez <smalldirkalex@gmail.com>
2025-03-27 16:17:27 +0800

    - update lambda_1.kt

--- snippets/lambda_1.kt
+++ snippets/lambda_1.kt
@@ -9,6 +9,9 @@ fun main() {
 
     // [Higher-order functions and lambdas | Kotlin Documentation](https://kotlinlang.org/docs/lambdas.html#it-implicit-name-of-a-single-parameter)
      { it.length == 5 }
+
+    var arr: IntArray = intArrayOf(1, 2, 3)
+    println("Hello, world!!! ${arr.size}")
     
     val list = listOf(1, 2, 3)
     println("Hello, world!!! ${list.filter({it > 1})}")
