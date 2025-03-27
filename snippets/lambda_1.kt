a72a61961fb2b6d9e18bff35560943baa8bd1927
Dirk Arnez <smalldirkalex@gmail.com>
2025-03-27 15:51:48 +0800

    - update lambda_1.kt

--- snippets/lambda_1.kt
+++ snippets/lambda_1.kt
@@ -5,6 +5,7 @@ fun main() {
 
     val a = { i: Int -> i + 1 } 
     fun(s: String): Int { return s.toIntOrNull() ?: 0 }
+    fun(x: Int, y: Int): Int = x + y
 
     // [Higher-order functions and lambdas | Kotlin Documentation](https://kotlinlang.org/docs/lambdas.html#it-implicit-name-of-a-single-parameter)
      { it.length == 5 }
